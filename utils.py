824c262d095a0435215e08fac7d7892449e01c18
100nuk <146798173+100nuk@users.noreply.github.com>
2023-12-31 10:51:12 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -326,3 +326,18 @@ def get_wish():
     else:
         status = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌘"
     return status
+
+async def admin_check(message: Message) -> bool:
+    if not message.from_user: return False
+    if message.chat.type not in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]: return False
+    if message.from_user.id in [777000, 1087968824]: return True
+    client = message._client
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    check_status = await client.get_chat_member(chat_id=chat_id,user_id=user_id)
+    admin_strings = [enums.ChatMemberStatus.OWNER, enums.ChatMemberStatus.ADMINISTRATOR]
+    if check_status.status not in admin_strings: return False
+    else: return True
+
+async def admin_filter(filt, client, message):
+    return await admin_check(message)
